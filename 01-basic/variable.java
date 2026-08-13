6eb1777a6b7d1f7f944659771045bcdad5bb89f3
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-14 00:56:21 +0530

    added variable program

---
+++ 01-basic/variable.java
@@ -0,0 +1,19 @@
+public class variable
+{
+	
+	public static void main(String[] args) 
+	{
+		String name="priya";
+		int age=20;
+		char Grade='A';
+		String Branch="computer engg";
+		double marks=9.05;
+		boolean learning=true;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(Grade);
+		System.out.println(Branch);
+		System.out.println(marks);
+		System.out.println(learning);
+		}
+	}
