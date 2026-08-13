7591bfd61fc5785bde37c0c8e22843b14040ce27
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-14 00:35:21 +0530

    add print and println statement program
    added example of print, println, and newline concepts in java

---
+++ 01-basic/print and println.java
@@ -0,0 +1,18 @@
+public class printstmt
+  {
+		public static void main(String[] args) 
+    {	
+		//only print concept
+		System.out.print("learning java");
+		System.out.print("firstprogram in java\n");
+		
+		// print\n concept
+		System.out.println("learning java");
+		System.out.println("firstprogram in java");
+		
+				//\n concept
+		System.out.println("hello \n  java");
+		System.out.println("java \n programming ");
+		}
+
+}
