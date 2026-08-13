8752af3b6037e877884952e7d49d4792a2e6dd48
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-13 23:31:05 +0530

    add java introduction program

---
+++ 01-basic/Introduction.java
@@ -0,0 +1,9 @@
+public class second 
+{
+public static void main(String[] args)
+  {
+		System.out.println("hello java");
+		System.out.println("my name is pooja");
+		System.out.println(" i am learning java");
+}
+}
