681479c5e136adc972e1dff2a9200fd4d9146a44
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-14 19:55:19 +0530

    add If Else example
    This program use if -else statement to check a condition and display the appropriate result

---
+++ 01-basic/09-ifelsecondition.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ifelsecondition
+{
+public static void main(String[] args) 
+  {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter age:");
+	int age= sc.nextInt();
+  	//if else condition
+	if(age>18) 
+	{
+		System.out.println("adult ");
+	}
+	else {
+		System.out.println("child");
+				}
+}
+}
