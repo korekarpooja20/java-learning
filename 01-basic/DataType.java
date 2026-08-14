68cd9f16a3b6cba186bd1ab9b922488b2d737fd9
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-14 15:29:54 +0530

    add Java Data Types Program
    added a basic Java program to demonstrate different Data Types such as int , float,  boolean ,double, short, long, char, byte and String.

---
+++ 01-basic/DataType.java
@@ -0,0 +1,63 @@
+public class Datatype 
+{
+	public static void main(String[] args)
+  {
+			// use of int DataType
+   	int a=10; // a is variable
+		int b=20; // b variable
+		int sum=a+b; //sum is variable
+		System.out.println(sum);
+		
+		//use of string  DataType
+		String name="pooja"; 
+		String Branch="Computer engg"; 
+		String Language="English"; 
+		System.out.println(name);
+		System.out.println(Branch);
+		System.out.println(Language);
+		
+		//use of float DataType
+		float marks=10.5f;
+		float value=20.5f;
+		System.out.println(marks);
+		System.out.println(value);
+		
+		// use of boolean DataType 
+		boolean p=true;
+		boolean q=false;
+		System.out.println(p);
+		System.out.println(q);
+		
+		//use of double DataType
+		double A= 50.40;
+		double B= 50.40;
+		System.out.println(A);
+		System.out.println(B);
+		
+		//use of char DataType
+		char Grade='A';
+    char Grade1='B';
+    char Grade2='C';
+    System.out.println(Grade);
+  	System.out.println(Grade1);
+    System.out.println(Grade2);
+		
+  	 //use of long DataType
+  	long S=200;
+  	long T=500;
+  	System.out.println(S);
+		System.out.println(T);
+		   
+	  //use of Short DataType
+	  short R=5600;
+	  short M=50;
+	  System.out.println(R);
+		System.out.println(M);
+			   
+		//use of byte DataType
+		 byte G=58;
+		 byte N=1;
+		 System.out.println(G);
+		 System.out.println(N);
+	}
+}
