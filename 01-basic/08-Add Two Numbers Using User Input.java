bed8fd6491d04d152e1e76e1768c1558da280dc2
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-14 17:32:12 +0530

    Create 08-Add Two Numbers Using User Input.java
    This program takes two numbers as input from the user using Scanner and calculates their sum using the + operator

---
+++ 01-basic/08-Add Two Numbers Using User Input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class userinput
+  {
+	public static void main(String[] args) 
+    {
+		Scanner sc=new Scanner(System.in);
+    System.out.println("Enter First Number:");
+		int a=sc.nextInt();
+    System.out.println("Enter Second Number:");
+		int b=sc.nextInt();
+		int sum=a+b;
+		System.out.println("Addition:"+sum);
+		}
+}
