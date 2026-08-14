f85f32da4030e6a1889726589b5c98d71568af6f
Pooja Korekar <korekarpooja20@gmail.com>
2026-08-14 20:24:59 +0530

    Create Positive Negative Number.java
    This program takes a number from the user and checks whether it is positive or negative using an if else statement.

---
+++ Positive Negative Number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class positive
+{
+public static void main(String[] args) 
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the Number:");
+	int num= sc.nextInt();
+	sc.nextLine();
+	
+	if(num<0) 
+	{
+		System.out.println("Negative Number");
+	}
+	else {
+		System.out.println("Positive Number");
+			}
+	 	}
+}
